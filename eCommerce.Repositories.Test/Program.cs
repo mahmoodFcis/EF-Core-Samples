23b847d48eb0b2c47ad7852db024328bc8ce3b06
mshaaban <mshaaban@DESKTOP-MLCAK3H>
2019-02-06 23:30:09 +0200

    adding mvc

--- eCommerce.Repositories.Test/Program.cs
+++ eCommerce.Repositories.Test/Program.cs
@@ -10,11 +10,11 @@ namespace eCommerce.Repositories.Test
         static void Main(string[] args)
         {
             ProductRepository productsRepo = new ProductRepository();
-            //var product = new Product() { Name = "Computer", Price = 3000, Category = new Category() { Name = "Electronics" } };
-            //productsRepo.Add(product);
+            var product = new Product() { Name = "Mobile", Price = 3000, Category = new Category() { Name = "Phones" } };
+            productsRepo.Add(product);
             //Console.WriteLine(product.Id);
 
-            var product = productsRepo.GetByView();
+            //var product = productsRepo.GetByView();
 
             //int numberOfAffectedRows=productsRepo.AddBySPFromContext("mobile", 100, 1);
         }
